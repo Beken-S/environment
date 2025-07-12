4d41b291cd083a73ce55bc07dfd2beef69eba405
Бекен Сарсенбаев <84242545+Beken-S@users.noreply.github.com>
2025-07-12 21:07:08 +0300

    Create disk-config.nix

---
+++ disk-config.nix
@@ -0,0 +1,53 @@
+{
+    disko.devices = {
+        disk = {
+          one = {
+            type = "disk";
+            device = "/dev/sda";
+            content = {
+              type = "gpt";
+              partitions = {
+                ESP = {
+                  size = "1GB";
+                  type = "EF00";
+                  content = {
+                    type = "filesystem";
+                    format = "vfat";
+                    mountpoint = "/boot";
+                    mountOptions = [ "umask=0077" ];
+                  };
+                };
+                lvm = {
+                  size = "100%";
+                  content = {
+                    type = "lvm_pv";
+                    vg = "vg_main";
+                  };
+                };
+              };
+            };
+          };
+        };
+    lvm_vg = {
+        vg_main = {
+            type = "lvm_vg";
+            lvs = {
+              swap = {
+                size = "8G";
+                content = {
+                  type = "swap";
+                };
+              };
+              root = {
+                size = "100%FREE";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                };
+              };
+            };
+          };
+        };
+    };
+}

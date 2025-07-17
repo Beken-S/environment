00d3df0bfecc8289435ce821de7154250657d9e7
Бекен Сарсенбаев <84242545+Beken-S@users.noreply.github.com>
2025-07-17 23:51:01 +0300

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+    inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    inputs.disko.url = "github:nix-community/disko/latest";
+    inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
+
+    outputs = { self, disko, nixpkgs }: {
+        nixosConfigurations.mymachine = nixpkgs.lib.nixosSystem [
+            ./configuration.nix
+            disko.nixosModules.disko
+            {
+                disko.devices = import ./disko-config.nix;
+            };
+        ];
+    };
+}
